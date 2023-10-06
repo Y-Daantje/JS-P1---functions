93319227b509702e2e9b838237a7c1b8e1567c4e
Y-Daantje <302867649@student.rocmondriaan.nl>
2023-10-06 12:55:33 +0200

    huiswerk

---
+++ js/main.js
@@ -0,0 +1,27 @@
+console.log('main log');
+
+//opdr1
+function greet () {
+    //console log does not work if you dont call the function 
+    console.log('hallo wereld');
+
+}
+greet();
+
+//opdr2
+
+function welcome() {
+    //textcontent use for to print it on your webpage with a funtion call
+    document.body.textContent = 'Welkom bij ROC Mondriaan'
+} 
+welcome();
+
+//opdr3
+function calculateSum(a, b) {
+    console.log('your answer is', a + b);
+    const answer = a + b;
+    
+    return answer;
+}
+const claculator = calculateSum(3, 4);
+calculateSum(30000, 6079707);
