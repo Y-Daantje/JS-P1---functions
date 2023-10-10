2773ab9c24937876d91eb6e848417574591dc312
Yannick <302867649@student.rocmondriaan.nl>
2023-10-11 00:08:25 +0200

    opdr6

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ welcome();
 
 //opdr3
 function calculateSum(a, b) {
-    console.log('your answer is', a + b);
+    console.log('your answer is---', a + b);
     const answer = a + b;
 
     return answer;
@@ -29,7 +29,7 @@ calculateSum(30000, 6079707);
 
 //opdr4
 function calculateDivision(e, t) {
-    console.log('your answer', e + t);
+    console.log('your answer...', e + t);
     const is = e + t;
 
     return is;
@@ -38,13 +38,34 @@ const result = calculateDivision(3, 4);
 
 
 //opdr5
-function showArrayOfNumbers(m) {
-    const Array = ['1', '2', '3', '4', '5']
-    for (let m = 0; m < array.length; m++) {
-        const element = array[m];
-        console.log(element);
+function showArrayOfNumbers() {
+    //variabel of nmr names with the arrays
+    const priemgetal = ['1', '2', '3', '4', '5'];
+    //for element make a variabel n value of 0 < to the var names
+    for (let n = 0; n < priemgetal.length; n++) {
+        //log the nmr value
+        console.log(priemgetal[n]);
     }
-return m;
 }
-const plz = showArrayOfNumbers (m);
+showArrayOfNumbers();
 
+//opdr6
+function showArrayOfBrands() {
+    //variabel of car names with the arrays
+    const automerken = ["Mercedes", "Volkswagen", "Peugeot", "Tesla", "Toyota"];
+    //for element make a variabel i value of 0 < to the var names
+    for (let i = 0; i < automerken.length; i++) {
+        //log the car names
+        console.log(automerken[i]);
+    }
+}
+
+// this function give out the array elements
+showArrayOfBrands();
+
+
+//opdr7
+let computer = 7;
+let player = 5;
+ let enterhogerlager = prompt('kies hoger of lager.....');
+ 
